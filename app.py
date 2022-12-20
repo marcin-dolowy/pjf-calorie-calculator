4a7eef476ec19d7f32b9c84ef3ff959d5eba796e
marcin-dolowy <marcindolowy00@gmail.com>
2022-12-20 01:54:41 +0100

    Remove useless import

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ import json
 import os
 import urllib.request
 from datetime import date
-import sys
 
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-import re
-import string
 
 from forms import RegistrationForm, LoginForm, RecipeForm
 
