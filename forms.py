2ca463df08f31320fa43e26c542b68bce79f3a51
marcin-dolowy <marcindolowy00@gmail.com>
2022-12-20 01:52:29 +0100

    Create forms for login, register and recipe

---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField, RadioField, IntegerField
+
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password1 = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password1')])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    date_of_birth = DateField('Date of birth', format='%Y-%m-%d', validators=[DataRequired()])
+    sex = RadioField('Gender', choices=["Male", "Female"], validators=[DataRequired()])
+    weight = IntegerField('Weight', validators=[DataRequired()])
+    height = IntegerField('Height', validators=[DataRequired()])
+    submit = SubmitField('Sign up')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class RecipeForm(FlaskForm):
+    products = StringField("Products", validators=[DataRequired()])
+    submit = SubmitField('Find')
